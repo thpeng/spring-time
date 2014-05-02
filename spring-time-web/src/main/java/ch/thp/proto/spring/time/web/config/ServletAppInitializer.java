fb13611af3f0ca9896ea844c12f9abd6512054c2
Thierry <thierry.peng@businessdecision.com>
2014-05-02 14:07:57 +0200

    spring, spring mvc setup success, deployable on Tomcat

---
+++ spring-time-web/src/main/java/ch/thp/proto/spring/time/web/config/ServletAppInitializer.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014 caleb.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.thp.proto.spring.time.web.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * Based on: http://kielczewski.eu/2013/11/spring-mvc-without-web-xml-using-webapplicationinitializer/
+ * 
+ * @author caleb
+ */
+public class ServletAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        WebApplicationContext context = getContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/*");
+    }
+
+    private AnnotationConfigWebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("ch.thp.proto.spring.time.web.config");
+        return context;
+    }
+
+}
---
+++ spring-time-web/src/main/java/ch/thp/proto/spring/time/web/config/SpringApplicationBaseConfig.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright Error: on line 4, column 29 in Templates/Licenses/license-apache20.txt
+ The string doesn't match the expected date/time format. The string to parse was: "02.05.2014". The expected format was: "MMM d, yyyy". caleb.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.thp.proto.spring.time.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Base spring configuration. Will load all other configurations
+ * @author caleb
+ */
+@Configuration
+//@ComponentScan(basePackages = "ch.thp.proto",excludeFilters={@ComponentScan.Filter(value = Controller.class), @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE ,value = SpringMVCRestConfig.class)})
+@ComponentScan(basePackages = "ch.thp.proto")
+public class SpringApplicationBaseConfig {
+
+
+}
